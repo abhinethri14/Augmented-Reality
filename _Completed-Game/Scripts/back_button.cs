22279329102cc15d4ce0eb910f9969bb633a4b1b
Abhinethri Tumkur Umesh <32636305+abhinethri14@users.noreply.github.com>
2019-01-28 13:08:13 -0700

    Add files via upload

---
+++ _Completed-Game/Scripts/back_button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using test;
+
+
+public class back_button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void back_action()
+    {
+        if (login.value == 0)
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Roll-a-ball");
+        else
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Roll-a-ball-cube");
+    }
+}
---
+++ _Completed-Game/Scripts/login.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace test
+{
+
+    public class login : MonoBehaviour
+    {
+
+
+        public Dropdown dropdown;
+        public static int value = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void navigate()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Roll-a-ball");
+        }
+
+        public void navigate_cube()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Roll-a-ball-cube");
+        }
+
+        public void navigate_decide()
+        {
+            // Debug.Log(dropdown.value);
+
+            value = dropdown.value;
+            if (value == 0)
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Roll-a-ball");
+            else
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Roll-a-ball-cube");
+        }
+    }
+}
